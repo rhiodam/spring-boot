573542e5f3808b90d776bec2d5ca5c42ce9b83cd
rhiodam <rhioda.m@gmail.com>
2019-02-22 05:40:31 +0700

    Update UserRepository.java

--- jpa-embeddable-demo/src/main/java/com/rhiodamuthie/jpaembeddabledemo/repository/UserRepository.java
+++ jpa-embeddable-demo/src/main/java/com/rhiodamuthie/jpaembeddabledemo/repository/UserRepository.java
@@ -4,10 +4,21 @@ import com.rhiodamuthie.jpaembeddabledemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // Find user by email
     User findByEmail(String email);
+
+    // Find users by firstName
+    List<User> findByNameFirstName(String firstName);
+
+    // Find users by lastName
+    List<User> findByNameLastName(String lastName);
+
+    // Find users by country
+    List<User> findByAddressCountry(String country);
 
 }
