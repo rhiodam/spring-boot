3dc52162d22872a3f8245ce251ca41f5347ce6d9
rhiodam <rhioda.m@gmail.com>
2019-02-22 17:54:20 +0700

    add pools-backend

---
+++ pools-backend/src/main/java/com/rhiodamuthie/poolsbackend/PoolsBackendApplication.java
@@ -0,0 +1,14 @@
+package com.rhiodamuthie.poolsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolsBackendApplication.class, args);
+	}
+
+}
+
