30381db51b1804d62a23cf747a34c68ecf2e2a74
rhiodam <rhioda.m@gmail.com>
2019-02-23 06:02:36 +0700

    finishing crud User

---
+++ spring-mvc-thymeleaf-user/src/main/java/com/rhiodamuthie/springmvcthymeleafuser/model/User.java
@@ -0,0 +1,30 @@
+package com.rhiodamuthie.springmvcthymeleafuser.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    // standard constructors / setters / getters / toString
+}
